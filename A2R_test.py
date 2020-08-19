dc2793c021b5868d2a2ba5030397b2942a5cb049
Zhichao_Lu <55032729+xxLin97@users.noreply.github.com>
2020-08-19 04:05:47 -0400

    Update A2R_test.py

--- A2R_test.py
+++ A2R_test.py
@@ -1,18 +1,20 @@
-import arabic2roman as a2r
+#import arabic2roman as a2r
+
+from A2R import arabic2roman
 
 def test_valid():               
-  assert a2rr.arabic2roman(1) == 'I';
-  assert a2r.arabic2roman(4) == 'IV';
-  assert a2r.arabic2roman(9) == 'IX';
-  assert a2r.arabic2roman(59) == 'LIX';
-  assert a2r.arabic2roman(94) == 'XCIV';
-  assert a2r.arabic2roman(999) == 'CMXCIX';
-  assert a2r.arabic2roman(1010) == 'MX';
-  assert a2r.arabic2roman(3999) == 'MMMCMXCIX';
+  assert arabic2roman(1) == 'I';
+  assert arabic2roman(4) == 'IV';
+  assert arabic2roman(9) == 'IX';
+  assert arabic2roman(59) == 'LIX';
+  assert arabic2roman(94) == 'XCIV';
+  assert arabic2roman(999) == 'CMXCIX';
+  assert arabic2roman(1010) == 'MX';
+  assert arabic2roman(3999) == 'MMMCMXCIX';
   
 def test_invalid():
-  assert a2r.arabic2roman('string') == 'Error'; 
-  assert a2r.arabic2roman(4.2) == 'Error';    
-  assert a2r.arabic2roman(-1) == 'Error';   
-  assert a2r.arabic2roman(0) == 'Error';
-  assert a2r.arabic2roman(4000) == 'Error';
+  assert arabic2roman('string') == 'Error'; 
+  assert arabic2roman(4.2) == 'Error';    
+  assert arabic2roman(-1) == 'Error';   
+  assert arabic2roman(0) == 'Error';
+  assert arabic2roman(4000) == 'Error';
